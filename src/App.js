9bdb043d695f96aa0a7d2ed93fb195c55e243a23
gootieno <geoff.otieno7@gmail.com>
2020-05-28 00:37:50 -0700

    finished a simple application teaching me how to work with various react components and how to manipulate them. This project included a few basic bootstrap styling as well.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Navbar from './components/NavBar';
+import Counters from './components/Counters';
 
-function App() {
-  return (
-    <h1>Hello world!</h1>
-  );
+class App extends Component{
+  state = {
+    counters: [
+        {id: 1, value: 0},
+        {id: 2, value: 0},
+        {id: 3, value: 0},
+        {id: 4, value: 0}
+    ]
+  };
+
+  handleDelete = (counterId) => {
+      const counters = this.state.counters.filter(counter => counter.id !== counterId)
+      this.setState({ counters })
+  }
+
+  handleReset = () => {
+      const counters = this.state.counters.map(counter => {
+        counter.value = 0
+        return counter
+      })
+      this.setState({ counters})
+  }
+
+  handleIncrement = counter => {
+      const counters = [...this.state.counters];
+      const index = counters.indexOf(counter);
+      counters[index] = {...counter};
+      counters[index].value++;
+      this.setState({counters})
+  }
+
+  render(){
+    return (
+      <>
+        <Navbar totalCounters={this.state.counters.filter(counter => counter.value > 0).length}/>
+        <main className='container'>
+          <Counters counters={this.state.counters} onReset={this.handleReset} onIncrement={this.handleIncrement} onDelete={this.handleDelete}/> 
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
